410b13c510ab357912341ef7847422c8f606f803
EmrahOkur <okur.emrah@web.de>
2025-07-17 17:22:40 +0200

    catching errors with console log

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@
 const haushaltsbuch ={
     
     gesamtbilanz: new Map(),
-
-
     eintraege:[],
+    fehler: [],
 
     eintrag_erfassen (){
         let neuer_eintrag= new Map();
@@ -14,7 +13,15 @@ const haushaltsbuch ={
         neuer_eintrag.set("betrag",this.betrag_verarbeiten(prompt("Betrag(in Euro):")));
         neuer_eintrag.set("datum", this.datum_verarbeiten(prompt("Datum (jjjj-mm-tt):")));
         neuer_eintrag.set("timestamp", Date.now());
-        this.eintraege.push(neuer_eintrag);
+        if (this.fehler.length === 0){
+            this.eintraege.push(neuer_eintrag);
+        } else {
+            console.log("Folgende Fehler wurden gefunden:")
+            this.fehler.forEach(function(fehler){
+                console.log(fehler);
+            });
+        }
+        
     },
 
      typ_verarbeiten(typ){
@@ -22,8 +29,7 @@ const haushaltsbuch ={
         if(this.typ_validieren(typ)) {
              return typ;
         } else {
-            console.log(`Ungültiger Eintrags-Typ "${typ}"`)
-            return false
+            this.fehler.push(`Ungültiger Eintrags-Typ "${typ}"`)
         }
     },
 
@@ -40,8 +46,7 @@ const haushaltsbuch ={
         if(this.titel_validieren(titel)) {
              return titel;
         } else {
-            console.log("kein titel angegeben")
-            return false
+            this.fehler.push("kein titel angegeben")
         }
     },
 
@@ -59,8 +64,7 @@ const haushaltsbuch ={
         if(this.betrag_validieren(betrag)) {
              return parseFloat(betrag.replace(",", "."))*100;
         } else {
-            console.log(`Ungültiger Betrag: ${betrag} €`)
-            return false
+            this.fehler.push(`Ungültiger Betrag: ${betrag} €`)
         }
     },
 
@@ -77,8 +81,7 @@ const haushaltsbuch ={
         if(this.datum_validieren(datum)) {
              return new Date(`${datum}T00:00:00`);
         } else {
-            console.log(`Ungültiger Datumsformat: "${datum}". `)
-            return false
+            this.fehler.push(`Ungültiger Datumsformat: "${datum}". `)
         }
     },
 
@@ -154,11 +157,16 @@ const haushaltsbuch ={
         let weiterer_eintrag = true;
         while (weiterer_eintrag){
             this.eintrag_erfassen();
+            if(this.fehler.length === 0){
             this.eintraege_sortieren();
             this.eintraege_ausgeben();
             this.gesamtbilanz_erstellen();
             this.gesamtbilanz_ausgeben();
+            } else {
+                this.fehler= [];
+            }
             weiterer_eintrag= confirm("Weiteren Eintrag hinzufügen?");
+            
     }
 }
 
