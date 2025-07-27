768081649465cd238dd351645e58816c02ce3aea
EmrahOkur <okur.emrah@web.de>
2025-07-27 13:27:00 +0200

    code refactoring

--- src/main.js
+++ src/main.js
@@ -19,10 +19,9 @@ const haushaltsbuch ={
             this.eintraege.push(neuer_eintrag);
         } else {
             console.log("Folgende Fehler wurden gefunden:")
-            this.fehler.forEach(function(fehler){
-                console.log(fehler);
-            });
+            this.fehler.forEach(fehler => console.log(fehler));    
         }
+        
         
     },
 
@@ -96,7 +95,7 @@ const haushaltsbuch ={
     },
 
     eintraege_sortieren(){
-        this.eintraege.sort(function(eintrag_a, eintrag_b){
+        this.eintraege.sort((eintrag_a, eintrag_b )=>{
             if (eintrag_a.get("datum") > eintrag_b.get("datum")){
                 return -1;
             } else if(eintrag_a.get("datum") < eintrag_b.get("datum")){
@@ -185,22 +184,18 @@ eintraege_ausgeben (){
     },
 
     gesamtbilanz_anzeigen(){
-        document.querySelectorAll("#gesamtbilanz").forEach(function(gesamtbilanz){
-            gesamtbilanz.remove();
-        });
+        document.querySelectorAll("#gesamtbilanz").forEach(gesamtbilanz=>
+            gesamtbilanz.remove());
         document.querySelector("body").insertAdjacentElement("beforeend", this.html_gesamtbilanz_generieren());
 
     },
 
      eintraege_anzeigen(){
-       document.querySelectorAll(".monatsliste ul").forEach(function(eintragsliste){
-            eintragsliste.remove();
-        });
+       document.querySelectorAll(".monatsliste ul").forEach(eintragsliste=>eintragsliste.remove());
 
         let eintragsliste=document.createElement("ul");
-        for (let eintrag of this.eintraege){
-            eintragsliste.insertAdjacentElement("beforeend", this.html_eintrag_generieren(eintrag))
-        }
+        this.eintraege.forEach(eintrag=>
+            eintragsliste.insertAdjacentElement("beforeend", this.html_eintrag_generieren(eintrag)));
         document.querySelector(".monatsliste").insertAdjacentElement("afterbegin", eintragsliste);
     },
 
@@ -253,14 +248,12 @@ eintraege_ausgeben (){
 },
 
 
-   
-
     gesamtbilanz_erstellen() {
         let neue_gesamtbilanz = new Map() 
         neue_gesamtbilanz.set("einnahmen", 0);
         neue_gesamtbilanz.set("ausgaben", 0);
         neue_gesamtbilanz.set("bilanz", 0);
-        this.eintraege.forEach(function(eintrag){
+        this.eintraege.forEach(eintrag=>{
              switch(eintrag.get("typ")){
             case "einnahme":
                 neue_gesamtbilanz.set("einnahmen", neue_gesamtbilanz.get("einnahmen") + eintrag.get("betrag"));
@@ -277,14 +270,6 @@ eintraege_ausgeben (){
        this.gesamtbilanz = neue_gesamtbilanz;
     },
 
-//    gesamtbilanz_ausgeben() {
-//     console.log(
-//         `Einnahmen: ${(this.gesamtbilanz.get("einnahmen") / 100).toFixed(2)} €\n` +
-//         `Ausgaben: ${(this.gesamtbilanz.get("ausgaben") / 100).toFixed(2)} €\n` +
-//         `Bilanz: ${(this.gesamtbilanz.get("bilanz") / 100).toFixed(2)} €\n` +
-//         `Bilanz ist positiv: ${(this.gesamtbilanz.get("bilanz") / 100) >= 0}`
-//     );
-// },
 
     eintraege_hinzufuegen(){
         let weiterer_eintrag = true;
